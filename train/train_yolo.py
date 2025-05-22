5d57604b0cd57ce0191b14ee7146ddd8e85f7c9f
Neirris <nepkka@gmail.com>
2025-05-22 22:44:13 +0500

    fix args

--- train/train_yolo.py
+++ train/train_yolo.py
@@ -6,23 +6,20 @@ import os
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    # Обязательные аргументы
     parser.add_argument("--data", type=str, required=True)
     parser.add_argument("--name", type=str, required=True)
-
     parser.add_argument("--model", type=str, default="assets/misc/yolo11n.pt")
-
-    # Параметры обучения
-    parser.add_argument("--epochs", type=int, default=30)
-    parser.add_argument("--imgsz", type=int, default=256)
-    parser.add_argument("--batch", type=int, default=16)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--imgsz", type=int, default=128)
+    parser.add_argument("--batch", type=int, default=32)
     parser.add_argument("--optimizer", type=str, default="AdamW")
-    parser.add_argument("--lr0", type=float, default=0.001)
+    parser.add_argument("--lr0", type=float, default=0.002)
     parser.add_argument("--lrf", type=float, default=0.01)
-    parser.add_argument("--warmup_epochs", type=int, default=3)
+    parser.add_argument("--warmup_epochs", type=int, default=2)
     parser.add_argument("--device", type=int, default=0)
     parser.add_argument("--pretrained", action="store_true")
-    parser.add_argument("--patience", type=int, default=10)
+    parser.add_argument("--patience", type=int, default=50)
+    parser.add_argument("--cache", type=str, default="disk")
     return parser.parse_args()
 
 
@@ -65,6 +62,7 @@ def main():
             pretrained=args.pretrained,
             patience=args.patience,
             project=save_dir,
+            cache=args.cache,
         )
 
     except Exception as e:
